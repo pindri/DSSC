f062f70556db81589b73da9b3cad5b875e18f745
pindri <patrick.indri@outlook.it>
2019-03-30 20:58:30 +0100

    reorganized assignment 1 files

--- Assignments_Indri/Assignment_1/Ex_1/code/run.sh
+++ Assignments_Indri/Assignment_1/Ex_1/code/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 module load openmpi/1.8.3/gnu/4.9.2 
-gcc -fopenmp pi.c cptimer.c -o exe.x -std=c11
+make benchmark
 > Output.dat
 
 for j in {1..4..1}
@@ -9,6 +9,6 @@ do
 for i in {1..20..1}
 do
 export OMP_NUM_THREADS=$i
-./exe.x >> Output.dat
+./benchmark.x >> Output.dat
 done
 done
